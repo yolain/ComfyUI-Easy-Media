007fa6ff85e6d8f420b9b51ac710c0f0d759c430
yolain <yolain@163.com>
2026-06-27 16:45:32 +0800

    chore: update release build artifacts

--- dist/release/chunks/index-emm6j89d.js
+++ dist/release/chunks/index-emm6j89d.js
@@ -1,2 +1,2 @@
 var g=()=>{if(typeof crypto<"u"&&typeof crypto.randomUUID==="function")return crypto.randomUUID();return`${Date.now().toString(36)}-${Math.random().toString(36).slice(2,11)}`};
-export{g as fa};
+export{g as ha};
