bc99fcc7f92dcade71327be897b2433ddbf12625
yolain <yolain@163.com>
2026-09-03 17:27:23 +0800

    chore: update release build

--- dist/release/chunks/index-4br27hat.js
+++ dist/release/chunks/index-4br27hat.js
@@ -1,2 +1,2 @@
 var B=()=>{if(typeof crypto<"u"&&typeof crypto.randomUUID==="function")return crypto.randomUUID();let c=crypto.getRandomValues(new Uint8Array(16));c[6]=c[6]&15|64,c[8]=c[8]&63|128;let j=Array.from(c,(z)=>z.toString(16).padStart(2,"0")).join("");return`${j.slice(0,8)}-${j.slice(8,12)}-${j.slice(12,16)}-${j.slice(16,20)}-${j.slice(20)}`};
-export{B as Ea};
+export{B as Sa};
